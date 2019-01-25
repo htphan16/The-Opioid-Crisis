8700e884afe1bcbe4562763f363da59f160261a7
Huong Phan <htphan16@earlham.edu>
2019-01-25 08:51:54 -0500

    Add files via upload

---
+++ Problem Investigation 1/MCM_NFLIS_Data.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Tentative approach/code to Problem Investigation 1
+# Not all results shown here will get into the paper
+
+drugs_df = pd.read_csv('MCM_NFLIS_Data.csv')
+groupby_state = drugs_df.groupby(['State'])
+groupby_county = drugs_df.groupby(['State', 'COUNTY'])
+groupby_year_state = drugs_df.groupby(['YYYY', 'State'])
+groupby_year_county = drugs_df.groupby(['YYYY', 'State', 'COUNTY'])
+
+# Over the years
+data_state_by_year = groupby_year_state.sum()['TotalDrugReportsState'].fillna(0)
+data_state_by_county = groupby_year_county.sum()['TotalDrugReportsCounty'].fillna(0)
+
+# States in order of decreasing number drug reports in 2010, including all data from 2010 to 2017
+def state_by_year():
+	state_by_year = pd.DataFrame()
+	for year in range(2010, 2018):
+		state_by_year[year] = data_state_by_year.sort_values(ascending=False)[year]
+		
+	return state_by_year
+
+state_by_year = state_by_year()
+print(state_by_year)
+
+# Counties in each state in order of decreasing number of drug reports in 2010, including all data from 2010 to 2017
+def county_by_state_by_year(state):
+	county_by_state_by_year = pd.DataFrame()
+	for year in range(2010, 2018):
+		county_by_state_by_year[year] = data_state_by_county.sort_values(ascending=False)[year][state]
+	return county_by_state_by_year
+
+
+county_by_oh_by_year = county_by_state_by_year('OH')
+county_by_pa_by_year = county_by_state_by_year('PA')
+county_by_va_by_year = county_by_state_by_year('VA')
+county_by_ky_by_year = county_by_state_by_year('KY')
+county_by_wv_by_year = county_by_state_by_year('WV')
+
+
+county_by_oh_by_year.to_csv('ohio_counties_sort_by_2010.csv', encoding='utf-8')
+county_by_pa_by_year.to_csv('pennsylvania_counties_sort_by_2010.csv', encoding='utf-8')
+county_by_va_by_year.to_csv('virginia_counties_sort_by_2010.csv', encoding='utf-8')
+county_by_ky_by_year.to_csv('kentucky_counties_sort_by_2010.csv', encoding='utf-8')
+county_by_wv_by_year.to_csv('west-virginia_counties_sort_by_2010.csv', encoding='utf-8')
+
+
+state_by_year.transpose().plot()
+state_by_year.plot.bar(stacked=True)
+plt.show()
+
+# Overall across all years
+# States in order of decreasing number of drug reports
+
+data_state_overall = groupby_state.sum()['TotalDrugReportsState'].fillna(0)
+state_overall = data_state_overall.sort_values(ascending=False)
+print(state_overall)
+
+data_county_overall = groupby_county.sum()['TotalDrugReportsCounty'].fillna(0)
+county_oh = data_county_overall.sort_values(ascending=False)['OH']
+county_pa = data_county_overall.sort_values(ascending=False)['PA']
+county_va = data_county_overall.sort_values(ascending=False)['VA']
+county_ky = data_county_overall.sort_values(ascending=False)['KY']
+county_wv = data_county_overall.sort_values(ascending=False)['WV']
+county_allstates = data_county_overall.sort_values(ascending=False)
+print("Ohio's top ten counties with the most drug reports in all years", county_oh.head(10))
+print("Pennsylvenia's top ten counties with the most drug reports in all years", county_pa.head(10))
+print("Virginia's top ten counties with the most drug reports in all years", county_va.head(10))
+print("Kentucky's top ten counties with the most drug reports in all years", county_ky.head(10))
+print("West Virgnia's top ten counties with the most drug reports in all years", county_wv.head(10))
+print("All 5 states's top ten counties with the most drug reports in all years", county_allstates.head(10))
+
+
+
